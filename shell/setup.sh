79a921f80d8ac28989eda22d08ba3d0bd3cc8472
Janne Wulf <jannewulf@gmail.com>
2019-10-29 19:37:02 +0100

    now using the command ln-backup directly, this is possible because it is sourced in calling scripts

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-NEWLINK="$HOME"/.profile
-
-if [ -f "$NEWLINK" ]; then
-  if [ -r "$NEWLINK" ] && [ -w $(dirname "$NEWLINK") ]; then
-    mv "$NEWLINK" "$NEWLINK".backup
-  else
-    echo "unable to backup $NEWLINK, check permissions. Not linked." >&2
-    exit 1
-  fi
-fi
-
-ln -s "$DOTFILES"/shell/profile.symlink $NEWLINK
+ln-backup "$DOTFILES"/shell/profile.symlink "$HOME"/.profile
 exit $?
