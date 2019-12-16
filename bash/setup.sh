95e290990a7a68a8a9de14536e67c0936e65a8a7
Janne Wulf <jannewulf@gmail.com>
2019-12-16 23:07:29 +0100

    initial bash setup

---
+++ bash/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -s "$DOTFILES"/bash/bashrc.symlink "$HOME"/.bashrc
+ln -s "$DOTFILES"/bash/bash_profile.symlink "$HOME"/.bash_profile
+ln -s "$DOTFILES"/bash/bash_aliases.symlink "$HOME"/.bash_aliases
