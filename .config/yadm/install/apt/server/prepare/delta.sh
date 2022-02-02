f79591f7f535205fdb845ead41f13a6973f61b13
Janne Wulf <jannewulf@gmail.com>
2022-02-02 11:59:35 +0100

    Use Github API to retrieve download link
    This makes the script more robust because parsing the JSON
    output is much more reliable than parsing the HTML site.

--- .config/yadm/install/apt/server/prepare/delta.sh
+++ .config/yadm/install/apt/server/prepare/delta.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-binary_link_local=$(curl -sSL https://github.com/dandavison/delta/releases/latest | \
-  grep -E -o "href=\".*git-delta_.+_$(dpkg-architecture -q DEB_BUILD_ARCH)\.deb\"" | \
-  cut -d '"' -f 2)
-binary_link=https://github.com${binary_link_local}
-deb_file=/tmp/$(basename $binary_link)
+if ! which dpkg-architecture > /dev/null; then
+  echo "dpkg-architecture was not found. Install package 'dpkg-dev' first" > /dev/stderr
+  exit 1
+fi
 
-curl -sSL $binary_link -o $deb_file && \
+if ! which jq > /dev/null; then
+  echo "jq was not found. Install it first" > /dev/stderr
+  exit 1
+fi
+
+system_type=$(dpkg-architecture -q DEB_BUILD_ARCH)
+deb_link=$(
+  wget -qO- https://api.github.com/repos/dandavison/delta/releases/latest | \
+  jq -r ".assets[] | select(.name | match(\"git-delta_.*_${system_type}.deb\")) | .browser_download_url"
+)
+
+if [ -z $deb_link ]; then
+  echo "Cannot find debian package for architecture '${system_type}'" > /dev/stderr
+  exit 1
+fi
+
+tmp_dir=$(mktemp -d)
+mkdir -p $tmp_dir
+deb_file=${tmp_dir}/$(basename $deb_link)
+
+wget -q $deb_link -O $deb_file && \
 sudo dpkg -i $deb_file
 
-rm -f $deb_file
+rm -rf $tmp_dir
 
