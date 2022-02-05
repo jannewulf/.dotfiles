0f10d17b4734b7189f4df59aa7ac41cccfe5576c
Janne Wulf <jannewulf@gmail.com>
2022-02-05 13:01:33 +0100

    Change to original open terminal extension

--- .local/share/nautilus-python/extensions/open-terminal.py
+++ .local/share/nautilus-python/extensions/open-terminal.py
@@ -1,10 +1,13 @@
-# https://gist.github.com/aunetx/f93fd4c2287d4670173fda747cb72623
+# https://gitlab.gnome.org/GNOME/nautilus-python/-/blob/master/examples/open-terminal.py
 
 import os
+
+# A way to get unquote working with python 2 and 3
 try:
     from urllib import unquote
 except ImportError:
     from urllib.parse import unquote
+
 import gi
 gi.require_version('GConf', '2.0')
 from gi.repository import Nautilus, GObject, GConf
@@ -13,37 +16,37 @@ from gi.repository import Nautilus, GObject, GConf
 class OpenTerminalExtension(Nautilus.MenuProvider, GObject.GObject):
     def __init__(self):
         self.client = GConf.Client.get_default()
-
+        
     def _open_terminal(self, file):
         filename = unquote(file.get_uri()[7:])
 
         os.chdir(filename)
-        os.system('kitty --detach')
-
+        os.system('gnome-terminal')
+        
     def menu_activate_cb(self, menu, file):
         self._open_terminal(file)
-
-    def menu_background_activate_cb(self, menu, file):
+        
+    def menu_background_activate_cb(self, menu, file): 
         self._open_terminal(file)
-
+       
     def get_file_items(self, window, files):
         if len(files) != 1:
             return
-
+        
         file = files[0]
         if not file.is_directory() or file.get_uri_scheme() != 'file':
             return
-
-        item = Nautilus.MenuItem(name='KittyTerminal::open_terminal_for_dir',
-                                 label='Open in Terminal',
-                                 tip='Open a Terminal in %s' % file.get_name())
+       
+        item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item',
+                                 label='Open Terminal' ,
+                                 tip='Open Terminal In %s' % file.get_name())
         item.connect('activate', self.menu_activate_cb, file)
         return item,
 
     def get_background_items(self, window, file):
-        item = Nautilus.MenuItem(name='KittyTerminal::open_terminal_current_dir',
-                                 label='Open in Terminal',
-                                 tip='Open a Terminal in %s' % file.get_name())
+        item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item2',
+                                 label='Open Terminal' ,
+                                 tip='Open Terminal In %s' % file.get_name())
         item.connect('activate', self.menu_background_activate_cb, file)
         return item,
 
